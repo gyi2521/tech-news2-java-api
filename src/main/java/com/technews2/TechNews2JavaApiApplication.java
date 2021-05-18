3fdb6233d3897341e03e4ec725025720eebe1500
nhyi <ginay2521@gmail.com>
2021-05-18 13:03:00 -0400

    initial files created with Spring Initializr

---
+++ src/main/java/com/technews2/TechNews2JavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.technews2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNews2JavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNews2JavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technews2/TechNews2JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technews2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNews2JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
